d704b5bd35423821c41815af082d06c4abe7440f
Alex <shebkoch@gmail.com>
2019-09-09 10:18:23 +0500

    fix styles

--- src/app/matches/matches.component.ts
+++ src/app/matches/matches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatchService} from "../match.service";
 
 @Component({
   selector: 'app-matches',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchesComponent implements OnInit {
 
-  constructor() { }
+  matches: any[];
+
+  constructor(private matchService: MatchService) { }
 
   ngOnInit() {
   }
